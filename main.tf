32c6839e16e005f1a7b1275eee5f33463e7ee307
Harishm2010 <145553810+Harishm2010@users.noreply.github.com>
2023-11-29 12:33:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,3 +45,20 @@ resource "snowflake_table" "demo_table" {
     comment = "Description for column2"
   }
 }
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
+
+resource "snowflake_file_format" "csv_file_format" {
+  name    = "CSV_FILE_FORMAT"
+  database = snowflake_database.demo_db.name
+  comment = "File format for CSV files"
+
+  type               = "CSV"
+  field_optionally_enclosed_by = "\""
+  record_delimiter   = "\n"
+  field_delimiter    = ","
+  skip_header        = 1
+}
