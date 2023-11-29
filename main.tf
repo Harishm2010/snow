92fa397bbd0fd5bcfb756c90d5b766e85237a680
Harishm2010 <145553810+Harishm2010@users.noreply.github.com>
2023-11-29 10:48:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,9 +22,4 @@ resource "snowflake_database" "demo_db1" {
   name    = "DEMO_DB1"
   comment = "Database for Snowflake Terraform demo1"
 }
-resource "snowflake_schema" "demo_schema" {
-  database = snowflake_database.demo_db1.name
-  name     = "DEMO_SCHEMA"
-  comment  = "Schema for Snowflake Terraform demo1"
-}
 
